280c5cd57410bdbe2bdefabc211add14b8c726d4
Daniel Rosenberg <daniel@ideliverable.com>
2018-03-14 17:03:58 +0100

    Added FileSizeFormatExtensions overload for formatting long values. Bumped version no 1.0.2.

--- IDeliverable.Utils.Core/FileSizeFormatExtensions.cs
+++ IDeliverable.Utils.Core/FileSizeFormatExtensions.cs
@@ -8,5 +8,10 @@ namespace IDeliverable.Utils.Core
         {
             return String.Format(new FileSizeFormatProvider(), "{0:fs}", value);
         }
+
+        public static string ToFileSizeString(this long value)
+        {
+            return String.Format(new FileSizeFormatProvider(), "{0:fs}", value);
+        }
     }
 }
