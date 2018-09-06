1ab571b785855683ff73eb51c7547f1295eb2882
Daniel Rosenberg <daniel@ideliverable.com>
2018-09-07 00:41:13 +0200

    Added additional XML extension methods.

--- IDeliverable.Utils.Core/XmlExtensions.cs
+++ IDeliverable.Utils.Core/XmlExtensions.cs
@@ -1,10 +1,23 @@
 using System;
+using System.IO;
+using System.Text;
+using System.Xml;
 using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace IDeliverable.Utils.Core
 {
     public static class XmlExtensions
     {
+        public static XmlNamespaceManager CreateNamespaceManager(this XNode xml, string prefix, string uri)
+        {
+            var nav = xml.CreateNavigator();
+            var nsm = new XmlNamespaceManager(nav.NameTable);
+            nsm.AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance"); // Required to check for xsi:null.
+            nsm.AddNamespace(prefix, uri);
+            return nsm;
+        }
+        
         public static string ReadAttribute(this XElement element, string name, Func<string> defaultValueFunc = null)
         {
             var attribute = element.Attribute(name);
@@ -23,6 +36,24 @@ namespace IDeliverable.Utils.Core
                 return defaultValueFunc != null ? defaultValueFunc() : default(T);
 
             return readFunc(value);
+        }
+
+        public static byte[] ToByteArray(this XElement element)
+        {
+            var settings = new XmlWriterSettings()
+            {
+                Encoding = Encoding.UTF8,
+                Indent = true,
+                NamespaceHandling = NamespaceHandling.OmitDuplicates
+            };
+
+            using (var s = new MemoryStream())
+            { 
+                using (var writer = XmlWriter.Create(s, settings))
+                    element.Save(writer);
+
+                return s.ToArray();
+            }
         }
     }
 }
