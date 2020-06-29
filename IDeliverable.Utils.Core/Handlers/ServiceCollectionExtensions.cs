54873b13bac6dc88266e3eafbdaa7524920e3994
Daniel Rosenberg <daniel@ideliverable.com>
2020-06-29 22:27:14 +0200

    Fixed some compiler warnings.

--- IDeliverable.Utils.Core/Handlers/ServiceCollectionExtensions.cs
+++ IDeliverable.Utils.Core/Handlers/ServiceCollectionExtensions.cs
@@ -5,54 +5,54 @@ using IDeliverable.Utils.Core.Handlers;
 
 namespace Microsoft.Extensions.DependencyInjection
 {
-	public static partial class ServiceCollectionExtensions
-	{
-		public static IServiceCollection AddHandler<TService>(this IServiceCollection services)
-		{
-			foreach (var implementedInterface in typeof(TService).GetInterfaces())
-			{
+    public static partial class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddHandler<TService>(this IServiceCollection services)
+        {
+            foreach (var implementedInterface in typeof(TService).GetInterfaces())
+            {
                 if (implementedInterface.IsConstructedGenericType && implementedInterface.GetGenericTypeDefinition() == typeof(IHandler<>))
                 {
-                    services.AddSingleton(implementedInterface, typeof(TService));
-                    services.AddHandlers(implementedInterface);
-                }
-			}
-
-			return services;
-		}
-
-		public static IServiceCollection AddHandler<TService>(this IServiceCollection services, TService implementationInstance)
-		{
-			foreach (var implementedInterface in typeof(TService).GetInterfaces())
-			{
-                if (implementedInterface.IsConstructedGenericType && implementedInterface.GetGenericTypeDefinition() == typeof(IHandler<>))
-                {
-                    services.AddSingleton(implementedInterface, implementationInstance);
-                    services.AddHandlers(implementedInterface);
+                    _ = services
+                        .AddSingleton(implementedInterface, typeof(TService))
+                        .AddHandlers(implementedInterface);
                 }
             }
 
-			return services;
-		}
+            return services;
+        }
+
+        public static IServiceCollection AddHandler<TService>(this IServiceCollection services, TService implementationInstance)
+        {
+            foreach (var implementedInterface in typeof(TService).GetInterfaces())
+            {
+                if (implementedInterface.IsConstructedGenericType && implementedInterface.GetGenericTypeDefinition() == typeof(IHandler<>))
+                {
+                    _ = services
+                        .AddSingleton(implementedInterface, implementationInstance)
+                        .AddHandlers(implementedInterface);
+                }
+            }
+
+            return services;
+        }
 
         public static IServiceCollection AddHandler<TMessage>(this IServiceCollection services, Action<TMessage> handler)
         {
             var delegateHandler = new DelegateHandler<TMessage>(handler);
 
-            services.AddSingleton<IHandler<TMessage>>(delegateHandler);
-            services.AddHandlers(typeof(IHandler<TMessage>));
-
-            return services;
+            return services
+                .AddSingleton<IHandler<TMessage>>(delegateHandler)
+                .AddHandlers(typeof(IHandler<TMessage>));
         }
 
         public static IServiceCollection AddHandler<TMessage>(this IServiceCollection services, Func<TMessage, Task> handler)
         {
             var delegateHandler = new DelegateHandler<TMessage>(handler);
 
-            services.AddSingleton<IHandler<TMessage>>(delegateHandler);
-            services.AddHandlers(typeof(IHandler<TMessage>));
-
-            return services;
+            return services
+                .AddSingleton<IHandler<TMessage>>(delegateHandler)
+                .AddHandlers(typeof(IHandler<TMessage>));
         }
 
         private static IServiceCollection AddHandlers(this IServiceCollection services, Type implementedInterface)
@@ -64,7 +64,7 @@ namespace Microsoft.Extensions.DependencyInjection
             if (!services.Any(serviceDescription => serviceDescription.ServiceType == handlersServiceType))
             {
                 var handlersImplementationType = typeof(Handlers<>).MakeGenericType(messageType);
-                services.AddSingleton(handlersServiceType, handlersImplementationType);
+                _ = services.AddSingleton(handlersServiceType, handlersImplementationType);
             }
 
             return services;
